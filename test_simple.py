8ee44b711ce4736ad792bd07548fc4add9158b9e
Michael Firman <mfirman@nianticlabs.com>
2019-06-11 14:15:59 +0100

    jpg -> jpeg (might be more pillow compatible)

--- test_simple.py
+++ test_simple.py
@@ -141,7 +141,7 @@ def test_simple(args):
             colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
             im = pil.fromarray(colormapped_im)
 
-            name_dest_im = os.path.join(output_directory, "{}_disp.jpg".format(output_name))
+            name_dest_im = os.path.join(output_directory, "{}_disp.jpeg".format(output_name))
             im.save(name_dest_im)
 
             print("   Processed {:d} of {:d} images - saved prediction to {}".format(
