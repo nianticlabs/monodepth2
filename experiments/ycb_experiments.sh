3d32c4053be401ffa3b1f08f223d78c6d4963e1f
Varun Agrawal <varagrawal@gmail.com>
2019-11-02 15:24:32 -0400

    updated shell script for ycb experiment

--- experiments/ycb_experiments.sh
+++ experiments/ycb_experiments.sh
@@ -1,2 +1,2 @@
 python ../train.py --model_name M_ycb \
-  --split ycb --dataset ycb --data_path ../ycb_data --png
+  --split ycb --dataset ycb --data_path ../ycb_data --png --frame_ids=0 --disable_automasking
