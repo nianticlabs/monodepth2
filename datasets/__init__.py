fd741e011a1c3562183d3ecad1a7f59b31cd10e8
Anderson Adon <andersonaadon@gmail.com>
2020-12-05 20:20:15 -0500

    Add NYUDataset to folder init properly

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,2 @@
-from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset, NYUDataset
+from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
+from nyu_dataset import NYUDataset
