0201151bf98760664f141628fc84babc029d3e5f
Parker Killion <pkillion99@gmail.com>
2022-03-02 11:45:58 -0800

    png to jpg

--- dataset_interface.py
+++ dataset_interface.py
@@ -156,8 +156,8 @@ class MyDataset(torch.utils.data.Dataset):
                         if num not in veloNums and num in RimageNums:
                             print("error")
                             print(i, num)
-                            errorFilesL += [f"{LFront}{num}.png"]
-                            errorFilesR += [f"{RFront}{num}.png"]
+                            errorFilesL += [f"{LFront}{num}.jpg"]
+                            errorFilesR += [f"{RFront}{num}.jpg"]
                     
                 
                 for image in LImages:
@@ -166,7 +166,7 @@ class MyDataset(torch.utils.data.Dataset):
                 print("\nerror files: ")
                 for image in errorFilesL:
                     print(image)
-                    
+
                 for L, R in zip(errorFilesL, errorFilesR):
                     LImages.remove(L)
                     RImages.remove(R)
