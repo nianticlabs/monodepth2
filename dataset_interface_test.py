4998c9e9ab64b9a6dfd6dfff519529c492acd065
Parker Killion <pkillion99@gmail.com>
2022-03-02 13:14:28 -0800

    test changes

--- dataset_interface_test.py
+++ dataset_interface_test.py
@@ -11,13 +11,13 @@ def main():
     dataset = MyDataset("train")
 
     loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1, shuffle=False)
-    depth = to_depth(1, dataset.baseline, dataset.focalLength)
-    print(f"depth from disp = 1: {depth}")
-    disparity = to_disparity(depth, dataset.baseline, dataset.focalLength)
-    print(f"to disparity using calced (should be 1): {disparity}")       
-
+    
     for tup in loader:
-
+        imgL, imgR, depth_gtL, depth_gtR, focalLength, baseline = tup
+        depth = to_depth(1, baseline, focalLength)
+        print(f"depth from disp = 1: {depth}")
+        disparity = to_disparity(depth, baseline, focalLength)
+        print(f"to disparity using calced (should be 1): {disparity}")       
         pdb.set_trace()
 
 
