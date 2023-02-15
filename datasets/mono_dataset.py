b2f0734c88641d041358db7a3712ccf1baeb303f
XinweiJu <117674985+XinweiJu@users.noreply.github.com>
2023-02-15 09:45:40 +0000

    Revert "Update mono_dataset.py"

--- datasets/mono_dataset.py
+++ datasets/mono_dataset.py
@@ -173,7 +173,7 @@ class MonoDataset(data.Dataset):
             inputs[("inv_K", scale)] = torch.from_numpy(inv_K)
 
         if do_color_aug:
-            color_aug = transforms.ColorJitter(
+            color_aug = transforms.ColorJitter.get_params(
                 self.brightness, self.contrast, self.saturation, self.hue)
         else:
             color_aug = (lambda x: x)
